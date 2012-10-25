709a07bb118af17ceaa80909254622ecdc6a99f2
Justin Edelson <justin@justinedelson.com>
2012-10-25 16:00:49 -0400

    updating version to 2.4.28

--- Library/Formula/filevault.rb
+++ Library/Formula/filevault.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Filevault < Formula
   homepage 'http://dev.day.com/docs/en/crx/current/how_to/how_to_use_the_vlttool.html'
-  url 'http://repo.adobe.com/archiva/repository/day-central/com/day/jcr/vault/vault-cli/2.4.26/vault-cli-2.4.26-assembly.tar.gz'
-  md5 'c6d99b70eebdacb4fa7101a4716e2753'
-  version '2.4.26'
+  url 'http://repo.adobe.com/archiva/repository/day-central/com/day/jcr/vault/vault-cli/2.4.28/vault-cli-2.4.28-assembly.tar.gz'
+  md5 '5caca0537343b147fb4c3d0773268d08'
+  version '2.4.28'
 
   def install
     # Remove windows files
